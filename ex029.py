582da89ae2e7afed78c5342e671edda55063f317
Mario Puebla Junior <mariopuebla17@gmail.com>
2022-09-28 21:50:44 -0300

    realizado mais 2 tarefas (até ex 29)

--- ex029.py
+++ ex029.py
@@ -1,10 +1,8 @@
 print('-=-' * 20)
 print('RADAR ELETRÔNICO \nLIMITE DE VELOCIDADE: 80KM/H')
 print('-=-' * 20)
-velocidade = int(input('Informe a velocidade do carro: '))
-limite = 80
-if velocidade <= limite:
-    print('Você está dentro da velocidade permitida!')
-else:
-    print('MULTADO! \nVocê excedeu o limite de velocidade em {} km/h'.format(velocidade - limite))
-    print('Sua multa é no valor de R${}'.format((velocidade - limite) * 7))
+velocidade = float(input('Informe a velocidade do carro: '))
+if velocidade > 80:
+    print('MULTADO! \nVocê excedeu o limite de velocidade em {} km/h'.format(velocidade - 80))
+    print('Sua multa é no valor de R${:.2f}'.format((velocidade - 80) * 7))
+print('Se beber não dirija!')
